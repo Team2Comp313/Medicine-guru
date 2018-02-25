c5bae016beed6d688c6131b3f9d7cadd999ff42c
Piyush5767 <piyush.sharma5767@gmail.com>
2018-02-24 21:57:50 -0500

    Product List Activity Dependency
    Include temporary product list activity dependency

---
+++ MedicineGuru/app/src/main/java/com/example/team2/medicineguru/Property.java
@@ -0,0 +1,34 @@
+package com.example.team2.medicineguru;
+
+/**
+ * Created by ASPIRE on 11-02-2018.
+ */
+
+//Base class to hold information about our property
+public class Property {
+
+    //property basics
+    private String title;
+    private String description;
+    private String image;
+    private Double price;
+    private Boolean featured;
+
+    //constructor
+    public Property(
+            String title, String description, Double price, String image, Boolean featured){
+
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.featured = featured;
+    }
+
+    //getters
+    public String getTitle() { return title; }
+    public String getDescription() {return description; }
+    public Double getPrice() {return price; }
+    public String getImage() { return image; }
+    public Boolean getFeatured(){return featured; }
+}
